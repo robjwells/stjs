e8532c180140c8b6363fcb85857d744d1a89fe0a
Rob Wells <rob@robjwells.com>
2021-12-20 01:25:15 +0000

    Ch02: line-count histogram exercise

---
+++ src/ch02-asynchronous-programming/x/lh.js
@@ -0,0 +1,31 @@
+import fs from "fs-extra-promise";
+import { filenamesFromArgv } from "./fileutils.js";
+
+const main = async () => {
+  const files = filenamesFromArgv(process.argv);
+  if (files.length === 0) return;
+  const fileCountPairs = await Promise.all(files.map(lineCount));
+  const counter = fileCountPairs.reduce((map, { lines }) => {
+    map.set(lines, (map.get(lines) ?? 0) + 1);
+    return map;
+  }, new Map());
+  const inOrder = Array.from(counter).sort(
+    ([lengthA, _a], [lengthB, _b]) => lengthA - lengthB
+  );
+
+  console.log("Length\tNumber of Files\n======\t===============");
+  for (const [length, nFiles] of inOrder) {
+    console.log(`${length}\t${nFiles} ${"*".repeat(nFiles)}`);
+  }
+};
+
+const lineCount = async (filename) => {
+  const data = await fs.readFileAsync(filename, { encoding: "utf-8" });
+  const lines = data.split("\n").length - 1;
+  return {
+    filename,
+    lines,
+  };
+};
+
+main();
